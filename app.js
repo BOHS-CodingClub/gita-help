3b28fddcbfdc6857128e9f415c4e35909e2f6c7a
DrPyxel <greenpyxel2@gmail.com>
2021-02-28 18:40:15 -0800

    Set up some chat stuff

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes/index.js';
 import path from 'path';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const __dirname = path.resolve(path.dirname(''));
 
@@ -11,6 +13,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 
-const server = app.listen(port, () => {
-    console.log(`App listening at https://localhost:${port}`);
+const server = app.listen(process.env.PORT || port, () => {
+    console.log(`App listening at https://localhost:${server.address().port}`);
 });
+
+
+// chat stuff
+const io = new Server(server);
+let users = {};
+io.on('connection', function (socket) {
+    socket.name = "User" + socket.id;
+    users[socket.id] = {
+        name:socket.name
+    };
+    io.emit('userInfo',
+        { 'name': socket.name }
+    );
+
+    socket.on('disconnect', function () {
+        users[socket.id] = {};
+    });
+})
