984470dcfbe64440a420e842bd297636bc6c55f5
DrPyxel <greenpyxel2@gmail.com>
2021-03-08 16:39:17 -0800

    Added message class, messages can now be sent to server from client

--- app.js
+++ app.js
@@ -21,16 +21,23 @@ const server = app.listen(process.env.PORT || port, () => {
 // Chat stuff VVV
 const io = new Server(server);
 let users = {}; // << List of all active users
-io.on('connection', function (socket) { // When a user connects
+let messages = [];
+setInterval(() => {
+    io.emit('updateMessage' , messages);
+}, 100);
+io.on('connection', function (socket) { // When a user connects VVV
     socket.name = "User" + socket.id; // Assigns the user an ID based on the order they joined
-    users[socket.id] = {
+    users[socket.id] = { // Pushes info to the array of active users
         name: socket.name
-    }; // Pushes info to the list of active users
-    io.emit('userInfo',
+    }; 
+    io.emit('userInfo', // sends the username to the client
         { 'name': socket.name }
-    ); // sends the username to the client
-
-    socket.on('disconnect', function () {
+    ); 
+    socket.on('sendMessage', function (arg) { // recieves sent messages from client
+        messages.push(arg.message);
+        console.log(arg);
+    }); 
+    socket.on('disconnect', function () { // removes disconnected users from the list
         users[socket.id] = {}; 
-    }); // removes disconnected users from the list
+    }); 
 });
--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,21 @@
 const socket = io(); // Initializes the socket connection to the server for the chatting app
+class Message { // This is an object for the message
+    constructor(message, sentBy, timeSent) {
+        this.message = message;
+        this.sentBy = sentBy;
+        this.timeSent = timeSent;
+    }
+}
 
 let userID; // User data
-socket.on('userInfo', function (data) {
+let messages; // messages
+
+socket.on('userInfo', function (data) { // Recieves user info sent by server 
     userID = data;
-}); // Recieves data sent by server 
+}); 
+socket.on('updateMessage', function (msgs) {
+    messages = msgs;
+});
+function sendMessage(messageArg) { // Sends message to server with the string messageArg
+    socket.emit('sendMessage', new Message(messageArg, userID, Date()));
+}
