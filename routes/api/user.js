9aa93932b04c16db050cbecdc88c8db5c3e1670a
Sachin Raja <sachinraja2349@gmail.com>
2021-03-16 13:47:13 -0700

    refactor: use dynamic url for user api

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,14 +3,8 @@ import User from '../../models/User.js';
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
-    if (!req.query.username) {
-        return res.status(400).json({
-            error: 'Username query is undefined.'
-        });
-    }
-
-    const user = await User.findOne({ username: req.query.username })
+router.get('/:username', async (req, res, next) => {
+    const user = await User.findOne({ username: req.params.username })
     .select({
         _id: 0,
         username: 1,
